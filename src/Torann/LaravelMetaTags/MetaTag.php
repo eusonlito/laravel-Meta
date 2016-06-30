21c4251ed2d21e3cc207f88d093cdea79883ba77
Robin C Samuel <robincharummoottil@gmail.com>
2016-06-30 20:05:44 +0530

    Update MetaTag.php

--- src/Torann/LaravelMetaTags/MetaTag.php
+++ src/Torann/LaravelMetaTags/MetaTag.php
@@ -3,6 +3,7 @@
 namespace Torann\LaravelMetaTags;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class MetaTag
 {
@@ -127,7 +128,7 @@ class MetaTag
     {
         return $this->createTag([
             'name' => $key,
-            'content' => $value?:$this->metas[$key]
+            'content' => $value ?: Arr::get($this->metas, $key, '')
         ]);
     }
 
